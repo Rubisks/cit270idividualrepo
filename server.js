3261cda20a08d3f5ea4c2ff13c033189bfdea67e
Tuker Strand <strand.tucker@gmail.com>
2022-05-03 09:55:02 -0600

    api changes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 
 const app = express();
+const port = 3000;
 
-app.listen(3000,()=>{console.log("listening...")});
+app.listen(port,()=>{console.log("listening on port: " + port)});
 
 app.get('/', (req,res)=>{res.send("Hello")});
