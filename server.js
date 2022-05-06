4067dc9f6796bde25d0030728acd0d3a1f2be390
Tuker Strand <strand.tucker@gmail.com>
2022-05-06 13:44:44 -0600

    started redis, fixed API and and was able to get welcome message

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require ('express');
+const bodyParser = require('body-parser');// body parser is called middleware
+const { response } = require('express');
 
+// crates an explress application we cant listen without.
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json())// use the middleware(call it before anthing else happens on each request)
+// call the listen through APP (express application)
 app.listen(port,()=>{console.log("listening on port: " + port)});
 
-app.get('/', (req,res)=>{res.send("Hello")});
-console.log('app')
+
+// calls get and if there are no path parameters ('/')
+app.get('/', (req,res)=>{res.send("Hello")});//every time somthing calls your API that is a request
+console.log('app');// responce is when the API responds with data requesteid
+
+
+app.post('/login',(request,response)=>{
+    const loginRequest = request.body;
+    if(loginRequest.userName =="callme@byui.edu" && loginRequest.password=="M@ckB00k"){
+        response.status(200);
+        response.send("welcome");
+    }
+    else{
+        response.status(401);
+        response.send("401 page not found");
+    }
+})// a post is when a client sends new information
+
+
+// calls get and if there are no path parameters ('/')
+app.get('/', (req,res)=>{res.send("Hello")});//every time somthing calls your API that is a request
+console.log('app');// responce is when the API responds with data requesteid
