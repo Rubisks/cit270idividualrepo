ececc2ee40709e3178815c379777526da22e5681
Tuker Strand <strand.tucker@gmail.com>
2022-06-22 11:54:22 -0600

    added socket info to server.js

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const { fstat } = require('fs');
 
 const redisClient = createClient(    
     {
-        url: 'redis://default @10.128.0.2:6379',
+        url: 'redis://default@10.128.0.2:6379',
+        socket:{
+            port:6379,
+            host:"127.0.0.1",
+        },
+        
     }
 );
 
