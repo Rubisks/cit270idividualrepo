c47eedbdfbac25cbb8be0d76353c11f68293325a
Tuker Strand <strand.tucker@gmail.com>
2022-06-27 12:52:36 -0600

    added new account creation scripts

--- cit270AuthenticationService/apicliant.sh
+++ cit270AuthenticationService/apicliant.sh
@@ -1,1 +1,1 @@
-curl  -d "@data.json" -X POST -H "Content-Type:application/json" https://tucker.cit270.com/login
+curl  -d "@$1" -X POST -H "Content-Type:application/json" https://tucker-node.cit270.com/login
---
+++ cit270AuthenticationService/signupandlogin.sh
@@ -0,0 +1,2 @@
+curl -d "@$1" -X POST -H "Content-Type:application/json" https://tucker-node.cit270.com/signup
+curl  -d "@$1" -X POST -H "Content-Type:application/json" https://tucker-node.cit270.com/login
